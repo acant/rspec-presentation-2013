20944e1d3772f6943bd1b12f2df13d246a620ac8
Andrew Sullivan Cant <acant@sugarcrm.com>
2013-03-26 00:12:15 -0400

    Handle one number in the input.

--- kata/spec/string_calculator_spec.rb
+++ kata/spec/string_calculator_spec.rb
@@ -4,6 +4,8 @@ describe StringCalculator do
 	describe '.add' do
 		subject { StringCalculator.add(numbers) }
 
-		context { let(:numbers) { '' } ; it { should == 0 } }
+		context { let(:numbers) { ''  } ; it { should == 0 } }
+		context { let(:numbers) { '1' } ; it { should == 1 } }
+		context { let(:numbers) { '5' } ; it { should == 5 } }
 	end
 end
