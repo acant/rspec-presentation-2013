73048030081212c86f1fb51ea854af681255f8b6
Andrew Sullivan Cant <acant@sugarcrm.com>
2013-03-26 10:10:01 -0400

    Handle an unknown number of numbers.

--- kata/lib/string_calculator.rb
+++ kata/lib/string_calculator.rb
@@ -1,13 +1,6 @@
 class StringCalculator
 	def self.add(numbers)
 		integers = numbers.split(',').map { |x| x.to_i }
-		case integers.length
-		when 0
-			0
-		when 1
-			integers[0].to_i
-		when 2
-			integers[0].to_i + integers[1].to_i
-		end
+		integers.inject(0) { |sum, number| sum.to_i + number.to_i}
 	end
 end
--- kata/spec/string_calculator_spec.rb
+++ kata/spec/string_calculator_spec.rb
@@ -4,9 +4,13 @@ describe StringCalculator do
 	describe '.add' do
 		subject { StringCalculator.add(numbers) }
 
-		context { let(:numbers) { ''  }   ; it { should == 0 } }
-		context { let(:numbers) { '1' }   ; it { should == 1 } }
-		context { let(:numbers) { '5' }   ; it { should == 5 } }
-		context { let(:numbers) { '1,2' } ; it { should == 3 } }
+		context { let(:numbers) { ''  }           ; it { should == 0 } }
+		context { let(:numbers) { '1' }           ; it { should == 1 } }
+		context { let(:numbers) { '5' }           ; it { should == 5 } }
+		context { let(:numbers) { '1,2' }         ; it { should == 3 } }
+		#This seems adequate to verify that we can accept an arbitrary number
+		#of numbers. However, we could do more. Running several tests which
+		#check a random number of numbers could do it.
+		context { let(:numbers) { '1,2,3,5,7,9' } ; it { should == 27 } }
 	end
 end
