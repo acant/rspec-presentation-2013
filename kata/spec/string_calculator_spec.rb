3ccd304c1a37eb0627a1012de59870bb4425cbed
Andrew Sullivan Cant <acant@sugarcrm.com>
2013-03-26 00:10:46 -0400

    Empty input, and 0 result.

--- kata/spec/string_calculator_spec.rb
+++ kata/spec/string_calculator_spec.rb
@@ -2,7 +2,8 @@ require 'string_calculator'
 
 describe StringCalculator do
 	describe '.add' do
-		subject { StringCalculator.add(nil) }
-		it { should be_nil }
+		subject { StringCalculator.add(numbers) }
+
+		context { let(:numbers) { '' } ; it { should == 0 } }
 	end
 end
